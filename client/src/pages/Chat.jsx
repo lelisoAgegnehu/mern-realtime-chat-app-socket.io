9bcfd079bfe973a214ff634ad2f56afb5bcbeba8
leliso <lelisoagegnehu491@gmail.com>
2023-06-19 16:52:21 +0300

    navigate to login

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Chat() {
+  const navigater = useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("user")) {
+      navigater("/login");
+    }
+  }, []);
   return <div>Chat</div>;
 }
 
