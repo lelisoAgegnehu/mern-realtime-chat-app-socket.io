734dc11e7a579763eba1d175f3ae0538f385bc4f
leliso <lelisoagegnehu491@gmail.com>
2023-06-20 12:11:03 +0300

    navigate to login and setavatart page

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,13 +1,37 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { allUserRoute } from "../utils/api";
 
 function Chat() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [contacts, setContacts] = useState([]);
   const navigater = useNavigate();
   useEffect(() => {
-    if (!localStorage.getItem("user")) {
-      navigater("/login");
+    async function getUserInfo() {
+      if (!localStorage.getItem("user")) {
+        navigater("/login");
+      } else {
+        setCurrentUser(await JSON.parse(localStorage.getItem("user")));
+      }
     }
+    getUserInfo();
   }, []);
+
+  useEffect(() => {
+    async function setUserInfo() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          axios.get(`${allUserRoute}/${currentUser._id}`).then(({ data }) => {
+            setContacts[data.users];
+          });
+        } else {
+          navigater("/setAvatar");
+        }
+      }
+    }
+    setUserInfo();
+  }, [currentUser]);
   return <div>Chat</div>;
 }
 
