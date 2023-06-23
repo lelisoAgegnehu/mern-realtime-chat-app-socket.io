6a7450b1b32d523988a01634a29df2593a39ad5e
leliso <lelisoagegnehu491@gmail.com>
2023-06-23 23:41:20 +0300

    add contact and welcome page

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -2,13 +2,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { allUserRoute } from "../utils/api";
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { Contacts } from "./Contacts";
+import { Welcome } from "./Welcome";
+import ChatContainer from "./ChatContainer";
 
 function Chat() {
   const [currentUser, setCurrentUser] = useState(null);
   const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(false);
   const navigater = useNavigate();
+  const theme = useTheme();
   useEffect(() => {
     async function getUserInfo() {
       if (!localStorage.getItem("user")) {
@@ -27,6 +32,7 @@ function Chat() {
           axios.get(`${allUserRoute}/${currentUser._id}`).then(({ data }) => {
             setContacts(data.users);
           });
+          setIsLoading(true);
         } else {
           navigater("/setAvatar");
         }
@@ -34,10 +40,25 @@ function Chat() {
     }
     setUserInfo();
   }, [currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
   return (
-    <Box margin="10%" bgcolor="darkgray">
-      <Box height="80%">
-        <Contacts contacts={contacts} currentUser={currentUser} />
+    <Box display="flex" justifyContent="center" alignItems="center" mt="10vh">
+      <Box bgcolor={theme.palette.primary.dark} height="80vh" width="80vw">
+        <Box display="flex">
+          <Contacts
+            changeChat={handleChatChange}
+            contacts={contacts}
+            currentUser={currentUser}
+          />
+          {isLoading && currentChat === undefined ? (
+            <Welcome currentUser={currentUser} />
+          ) : (
+            <ChatContainer currentChat={currentChat} />
+          )}
+        </Box>
       </Box>
     </Box>
   );
