e6746dd199b1fb0fb37dbe81800f7ca8c25a8867
leliso <lelisoagegnehu491@gmail.com>
2023-07-07 22:43:31 +0300

    add chat container

--- client/src/pages/ChatContainer.jsx
+++ client/src/pages/ChatContainer.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
 
-const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+const ChatContainer = ({ currentChat }) => {
+  console.log({ currentChat });
+  return (
+    currentChat && (
+      <Box display="flex" alignItems="center" gap={1}>
+        <Box>
+          <img
+            src={`data:image/svg+xml;base64,${currentChat.avatarImage}`}
+            alt="avatar"
+            style={{
+              height: "50px",
+              borderRadius: "50%",
+              padding: "2px",
+            }}
+          />
+        </Box>
+        <Box>
+          <Typography variant="body1" color="white">
+            {currentChat.userName}
+          </Typography>
+        </Box>
+      </Box>
+    )
+  );
 };
 
 export default ChatContainer;
